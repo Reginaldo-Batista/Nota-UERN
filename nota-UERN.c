8c537381f44e6396051ffecbb189cb0994fa2ad3
Reginaldo-Batista <reginaldovlog@gmail.com>
2024-05-14 18:32:02 -0300

    Update 2.3
    Usando as funções existentes para armazenar a maior parte do código da main(), tornando o código mais facilmente alterável e legível.

--- nota-UERN.c
+++ nota-UERN.c
@@ -2,29 +2,48 @@
 #define nota_necessaria 7.0 //Caso a média final da UERN mude, só precisará alterar este valor manualmente.
 
 //Função que calcula quanto o aluno deve obter na 2ª nota para não ficar de 4ª prova.
-float f_previsao_n1(float n1){
-    return ((105 - 60 - n1 * 4) / 5.0);
-}
-
-//Função que calcula quanto o aluno precisará na 3ª avaliação.
-float f_qnt_preciso(float n1, float n2){
-    return ((105.0 - n1 * 4 - n2 * 5) / 6.0);
+void f_previsao_n1(float n1){
+    float precisa_segunda = ((105 - 60 - n1 * 4) / 5.0);
+    printf("\nDeve tirar pelo menos\nSegunda prova: %.2f\nTerceira prova: 10.00\n\n", precisa_segunda);
 }
 
 //Função que calcula a nota final aplicando os pesos 4, 5 e 6.
-float f_media_final(float n1, float n2, float n3){ 
-    return ((n1 * 4 + n2 * 5 + n3 * 6) / 15.0);
+void f_media_final(float n1, float n2, float n3){
+    float media = ((n1 * 4 + n2 * 5 + n3 * 6) / 15.0);
+    float f_nota_quarta_prova = 12 - media;
+    printf("\nSua nota final: %.2f\n", media);
+    if(media >= nota_necessaria)
+        printf("Passou! Parabens!\n\n");
+    else if(f_nota_quarta_prova > 10){
+        printf("Infelizmente esta reprovado! Tente novamente no proximo periodo.\n");
+        printf("Voce precisaria tirar na quarta prova: %.2f\n\n", f_nota_quarta_prova);
+    }
+    else{
+        printf("Ficou de quarta prova!\n");
+        printf("Precisara tirar na quarta prova: %.2f\n\n", f_nota_quarta_prova);
+    }
 }
 
 //Função que calcula qual nota o aluno precisará para a 4ª avaliação.
 float f_nota_quarta(float n1, float n2, float n3){
-    return 12 - f_media_final(n1, n2, n3);
+    return (12 - (n1 * 4 + n2 * 5 + n3 * 6) / 15.0);
+}
+
+//Função que calcula quanto o aluno precisará na 3ª avaliação.
+void f_qnt_preciso(float n1, float n2){
+    float nota_2_necessaria = ((105.0 - n1 * 4 - n2 * 5) / 6.0);
+    printf("\nPrecisara de: %.2f\n", nota_2_necessaria);
+    if(nota_2_necessaria <= 10)
+        printf("Da pra passar!\n\n");
+    else{
+        printf("Ficou de quarta prova!\n");
+        printf("Se tirar 10.00 na terceira, precisara tirar na quarta prova: %.2f\n\n", f_nota_quarta(n1, n2, 10));
+    }
 }
 
 int main(){
 
     float nota1, nota2, nota3;
-    float media, f_nota_quarta_prova, nota_2_necessaria;
     int escolha, repetidor = 1; //Repetidor recebe inicialmente 1, que equivale a 'verdadeiro', abrindo o loop.
 
     do{
@@ -39,7 +58,7 @@ int main(){
             case 1:
                 printf("\nNota prova 1: ");
                 scanf("%f", &nota1);
-                printf("\nDeve tirar pelo menos\nSegunda prova: %.2f\nTerceira prova: 10.00\n\n", f_previsao_n1(nota1));
+                f_previsao_n1(nota1);
             break;
 
             case 2:
@@ -47,16 +66,8 @@ int main(){
                 scanf("%f", &nota1);
                 printf("Nota prova 2: ");
                 scanf("%f", &nota2);
-                nota_2_necessaria = f_qnt_preciso(nota1, nota2);
-                printf("\nPrecisara de: %.2f\n", nota_2_necessaria);
-
-                if(nota_2_necessaria <= 10)
-                    printf("Da pra passar!\n\n");
-                else{
-                    printf("Ficou de quarta prova!\n");
-                    f_nota_quarta_prova = f_nota_quarta(nota1, nota2, 10);
-                    printf("Se tirar 10.00 na terceira, precisara tirar na quarta prova: %.2f\n\n", f_nota_quarta_prova);
-                }
+                f_qnt_preciso(nota1, nota2);
+                
             break;
 
             case 3:
@@ -66,20 +77,7 @@ int main(){
                 scanf("%f", &nota2);
                 printf("Nota prova 3: ");
                 scanf("%f", &nota3);
-                media = f_media_final(nota1, nota2, nota3);
-                f_nota_quarta_prova = f_nota_quarta(nota1, nota2, nota3);
-                printf("\nSua nota final: %.2f\n", media);
-
-                if(media >= nota_necessaria)
-                    printf("Passou! Parabens!\n\n");
-                else if(f_nota_quarta_prova > 10){
-                    printf("Infelizmente esta reprovado! Tente novamente no proximo periodo.\n");
-                    printf("Voce precisaria tirar na quarta prova: %.2f\n\n", f_nota_quarta_prova);
-                }
-                else{
-                    printf("Ficou de quarta prova!\n");
-                    printf("Precisara tirar na quarta prova: %.2f\n\n", f_nota_quarta_prova);
-                }
+                f_media_final(nota1, nota2, nota3);
             break;
 
             case 4:
